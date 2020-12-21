f1bc6b0ac9e28c7c39e95e9f2c94ef3a96c1914c
Gabriel Henrique Kuhn <67128059+gabrielkuhn@users.noreply.github.com>
2020-12-20 22:07:23 -0300

    create password recovery tests

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -3,7 +3,7 @@ import User from '@modules/users/infra/typeorm/entities/User'
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO'
 import { v4 as uuid } from 'uuid'
 
-class UsersRepository implements IUsersRepository {
+class FakeUsersRepository implements IUsersRepository {
   private users: User[] = []
 
   public async findById(id: string): Promise<User | undefined> {
@@ -41,4 +41,4 @@ class UsersRepository implements IUsersRepository {
   }
 }
 
-export default UsersRepository
+export default FakeUsersRepository
--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -3,7 +3,7 @@ import AppError from '@shared/errors/AppError'
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import UpdateUserAvatarService from './UpdateUserAvatarService'
 
-describe('CreateUser', () => {
+describe('UpdateUserAvatar', () => {
   it('should be able to update the avatar', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
     const fakeStorageProvider = new FakeStorageProvider()
