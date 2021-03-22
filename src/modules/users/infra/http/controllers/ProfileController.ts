2ce4d84f5fcb3ca59f0527def9b321a67d9fcf14
Gabriel Henrique Kuhn <67128059+gabrielkuhn@users.noreply.github.com>
2021-03-22 02:10:00 -0300

    use class transform to change the controllers returns

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
-
+import { classToClass } from 'class-transformer'
 import UpdateProfileService from '@modules/users/services/UpdateProfileService'
 import ShowProfileService from '@modules/users/services/ShowProfileService'
 
@@ -29,12 +29,6 @@ export default class ProfileController {
       password,
     })
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    }
-
-    return response.json(userWithoutPassword)
+    return response.json(classToClass(user))
   }
 }
--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,6 +1,7 @@
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
 import { Request, Response } from 'express'
+import { classToClass } from 'class-transformer'
 import { container } from 'tsyringe'
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
 
 export default class SessionsController {
   public async update(request: Request, response: Response): Promise<Response> {
@@ -11,15 +12,6 @@ export default class SessionsController {
       avatarFilename: request.file.filename,
     })
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    }
-
-    return response.json(userWithoutPassword)
+    return response.json(classToClass(user))
   }
 }
