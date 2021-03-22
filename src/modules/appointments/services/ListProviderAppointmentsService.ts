227653554c51f387dad5573ccb53ea75f688674b
Gabriel Henrique Kuhn <67128059+gabrielkuhn@users.noreply.github.com>
2021-03-22 16:41:48 -0300

    minor adjustments

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -4,6 +4,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 import Appointment from '../infra/typeorm/entities/Appointment'
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
+import { classToClass } from 'class-transformer'
 
 interface IRequest {
   provider_id: string
@@ -42,7 +43,7 @@ class ListProviderAppointmentsService {
         },
       )
 
-      await this.cacheProvider.save(cacheKey, appointments)
+      await this.cacheProvider.save(cacheKey, classToClass(appointments))
     }
 
     return appointments
--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe'
-import { getDate, getDaysInMonth } from 'date-fns'
+import { getDate, getDaysInMonth, isBefore } from 'date-fns'
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 
@@ -44,13 +44,16 @@ class ListProviderMonthAvailabilityService {
     )
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59)
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day
       })
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isBefore(new Date(), compareDate) && appointmentsInDay.length < 10,
       }
     })
 
