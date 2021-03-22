486f2f11b6b66f1bb47151ad4407b7650a8fc305
Gabriel Henrique Kuhn <67128059+gabrielkuhn@users.noreply.github.com>
2021-03-22 04:36:37 -0300

    create AWS S3 storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,48 @@
-import multer from 'multer'
+import multer, { StorageEngine } from 'multer'
 import path from 'path'
 import crypto from 'crypto'
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 const uploadsFolder = path.resolve(tmpFolder, 'uploads')
 
+interface IUploadConfig {
+  driver: 's3' | 'disk'
+  tmpFolder: string
+  uploadsFolder: string
+
+  multer: {
+    storage: StorageEngine
+  }
+  config: {
+    disk: Record<string, unknown>
+    aws: {
+      bucket: string
+    }
+  }
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER || 'disk',
+
   tmpFolder,
   uploadsFolder,
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex')
-      const fileName = `${fileHash}-${file.originalname}`
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex')
+        const fileName = `${fileHash}-${file.originalname}`
 
-      return callback(null, fileName)
+        return callback(null, fileName)
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber',
     },
-  }),
-}
+  },
+} as IUploadConfig
