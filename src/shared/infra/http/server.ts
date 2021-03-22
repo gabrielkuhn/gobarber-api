5d845fa6fffa0ce3352f3227192bc8718a1d1928
Gabriel Henrique Kuhn <67128059+gabrielkuhn@users.noreply.github.com>
2021-03-22 01:27:17 -0300

    create validations for the routes endpoints

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import routes from './routes'
 
 import '@shared/infra/typeorm'
 import '@shared/container'
+import { errors } from 'celebrate'
 
 const app = express()
 
@@ -15,6 +16,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
+app.use(errors())
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
